15f66e928d5edbdccaa9b3c0d6eaffc4e5089293
Alevinluk <alevinluk@gmail.com>
2021-09-17 17:38:21 +0200

    Servidor de heroku

--- heroku-server.js
+++ heroku-server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-on-heroku'));
+app.use(express.static(__dirname + '/dist/gestor-crm'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/gestor-crm/index.html'));});
